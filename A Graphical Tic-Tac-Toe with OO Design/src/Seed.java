790825b2980af16a24b53097edd01fd81ff37e02
RAFIF TAQIY <rafiftaqiy05@gmail.com>
2025-06-09 20:18:09 +0700

    add A Graphical Tic-Tac-Toe with OO Design

---
+++ A Graphical Tic-Tac-Toe with OO Design/src/Seed.java
@@ -0,0 +1,8 @@
+/**
+ * This enum is used by:
+ * 1. Player: takes value of CROSS or NOUGHT
+ * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
+ */
+public enum Seed {
+    CROSS, NOUGHT, NO_SEED
+}
---
+++ A Graphical Tic-Tac-Toe with OO Design/src/State.java
@@ -0,0 +1,6 @@
+/**
+ * The enum State contains the various game states of the TTT game
+ */
+public enum State {
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
